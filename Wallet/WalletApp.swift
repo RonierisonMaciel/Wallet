d62d9dbadae96ae86942021a53783b86d253a659
ronierisonmaciel <ronierison.maciel@gmail.com>
2023-07-27 18:05:29 -0300

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Ronierison Maciel on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
