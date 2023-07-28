5ebcc47028c7daa493bd4df0edaf1ce406166477
ronierisonmaciel <ronierison.maciel@gmail.com>
2023-07-28 09:37:41 -0300

    update

---
+++ Wallet/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Wallet
+//
+//  Created by Ronierison Maciel on 27/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Olá, mundo!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
