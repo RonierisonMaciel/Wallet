0d0d3b38b937244c8f22f525cbbf3c43c78de418
ronierisonmaciel <ronierison.maciel@gmail.com>
2023-07-29 13:10:16 -0300

    update app wallet

---
+++ Wallet/view/AppState.swift
@@ -0,0 +1,20 @@
+//
+//  AppState.swift
+//  Wallet
+//
+//  Created by Ronierison Maciel on 29/07/23.
+//
+
+import SwiftUI
+
+struct AppState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppState_Previews: PreviewProvider {
+    static var previews: some View {
+        AppState()
+    }
+}
